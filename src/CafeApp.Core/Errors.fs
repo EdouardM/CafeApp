f8f9fc70bbd2fff735f027e529eec42e094b7f3a
Edouard Moureaux <edouard.moureaux@gmail.com>
2017-04-21 22:03:16 +0200

    Added Error types & Tests for ServeFoods transition

--- src/CafeApp.Core/Errors.fs
+++ src/CafeApp.Core/Errors.fs
@@ -10,14 +10,20 @@ namespace CafeApp
             | OrderAlreadyPlaced
             //Possible errors when Drink Served
             | CannotServeNonOrderedDrink of Drink
+            | CanNotServeAlreadyServedDrink of Drink
             
-            //Possible errors when Food Served
+            //Possible errors when Food Prepared
             | CannotPrepareNonOrderedFood of Food
             | CannotPrepareFoodForNonPlacedOrder
             | CannotPrepareFoodForClosedTab
+            | CanNotPrepareAlreadyPreparedFood of Food
 
+            //Possible errors when Food Prepared
             | CannotServeNonPreparedFood of Food
-            
+            | CannotServeFoodForNonPlacedOrder
+            | CannotServeFoodForClosedTab
+            | CanNotServeAlreadyServedFood of Food
+
 
             //Possible errors when Food or Drink Served
             | OrderAlreadyServed
